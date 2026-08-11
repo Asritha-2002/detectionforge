12e5272b30aad36897cf86d90fd06fd9debf948d
Saladi Asritha Devi <saladiasrithadevi@gmail.com>
2026-08-11 11:59:46 +0530

    afteradding vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { HelmetProvider } from "react-helmet-async";
-
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
    <HelmetProvider>
     <App />
+     <Analytics />
   </HelmetProvider>
   </BrowserRouter>
   </StrictMode>,
